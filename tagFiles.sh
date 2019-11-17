8b6407c715073fcc8e59bc2174fd9f4ecfebbdd0
1000TurquoisePogs <sgrady@rocketsoftware.com>
2019-11-17 15:13:58 -0500

    Do not use iname, does not seem supported on zos
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- tagFiles.sh
+++ tagFiles.sh
@@ -2,9 +2,9 @@
 # This program and the accompanying materials are
 # made available under the terms of the Eclipse Public License v2.0 which accompanies
 # this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-# 
+#
 # SPDX-License-Identifier: EPL-2.0
-# 
+#
 # Copyright Contributors to the Zowe Project.
 
 #TODO: add specific files without extensions
@@ -23,112 +23,190 @@ echo "Tagging files for ISO8859-1"
 
 find . \( \
 -name "*.1" -o \
--iname "AUTHORS" -o \
--iname ".babelrc" -o \
--iname "*.bat" -o \
--iname "*.bnf" -o \
--iname "*.bsd" -o \
--iname "*.cer" -o \
--iname "CODEOWNERS" -o \
--iname "*.coffee" -o \
--iname "*.conf" -o \
--iname "*.def" -o \
--iname ".editorconfig" -o \
--iname "*.el" -o \
--iname ".eslintignore" -o \
--iname ".eslintrc" -o \
--iname "*.gradle" -o \
--iname "*.in" -o \
--iname "*.info" -o \
--iname "*.js" -o \
--iname "Jenkinsfile" -o \
--iname ".jscsrc" -o \
--iname ".jshintignore" -o \
--iname ".jshintrc" -o \
--iname "*.json" -o \
--iname "*.jst" -o \
--iname "*.key" -o \
--iname "LICENSE" -o \
--iname "*.lock" -o \
--iname "*.log" -o \
--iname "*.ls" -o \
--iname ".mailmap" -o \
--iname "Makefile" -o \
--iname "*.map" -o \
--iname "*.markdown" -o \
--iname "*.md" -o \
--iname ".npmignore" -o \
--iname ".nycrc" -o \
--iname "*.opts" -o \
--iname "*.patch" -o \
--iname "*.ppf" -o \
--iname "*.properties" -o \
--iname "README" -o \
--iname "*.targ" -o \
--iname ".tm_properties" -o \
--iname "*.ts" -o \
--iname "*.tst" -o \
--iname "*.txt" -o \
--iname "*.xml" -o \
--iname "*.yaml" -o \
--iname "*.yml" \
+-name "AUTHORS" -o \
+-name ".babelrc" -o \
+-name ".BABELRC" -o \
+-name "*.bat" -o \
+-name "*.BAT" -o \
+-name "*.bnf" -o \
+-name "*.BNF" -o \
+-name "*.bsd" -o \
+-name "*.BSD" -o \
+-name "*.cer" -o \
+-name "*.CER" -o \
+-name "CODEOWNERS" -o \
+-name "*.coffee" -o \
+-name "*.COFFEE" -o \
+-name "*.conf" -o \
+-name "*.CONF" -o \
+-name "*.def" -o \
+-name "*.DEF" -o \
+-name ".editorconfig" -o \
+-name ".EDITORCONFIG" -o \
+-name "*.el" -o \
+-name "*.EL" -o \
+-name ".eslintignore" -o \
+-name ".ESLINTIGNORE" -o \
+-name ".eslintrc" -o \
+-name ".ESLINTRC" -o \
+-name "*.gradle" -o \
+-name "*.GRADLE" -o \
+-name "*.in" -o \
+-name "*.IN" -o \
+-name "*.info" -o \
+-name "*.INFO" -o \
+-name "*.js" -o \
+-name "*.JS" -o \
+-name "Jenkinsfile" -o \
+-name ".jscsrc" -o \
+-name ".JSCSRC" -o \
+-name ".jshintignore" -o \
+-name ".JSHINTIGNORE" -o \
+-name ".jshintrc" -o \
+-name ".JSHINTRC" -o \
+-name "*.json" -o \
+-name "*.JSON" -o \
+-name "*.jst" -o \
+-name "*.JST" -o \
+-name "*.key" -o \
+-name "*.KEY" -o \
+-name "LICENSE" -o \
+-name "*.lock" -o \
+-name "*.LOCK" -o \
+-name "*.log" -o \
+-name "*.LOG" -o \
+-name "*.ls" -o \
+-name "*.LS" -o \
+-name ".mailmap" -o \
+-name ".MAILMAP" -o \
+-name "Makefile" -o \
+-name "*.map" -o \
+-name "*.MAP" -o \
+-name "*.markdown" -o \
+-name "*.MARKDOWN" -o \
+-name "*.md" -o \
+-name "*.MD" -o \
+-name ".npmignore" -o \
+-name ".NPMIGNORE" -o \
+-name ".nycrc" -o \
+-name ".NYCRC" -o \
+-name "*.opts" -o \
+-name "*.OPTS" -o \
+-name "*.patch" -o \
+-name "*.PATCH" -o \
+-name "*.ppf" -o \
+-name "*.PPF" -o \
+-name "*.properties" -o \
+-name "*.PROPERTIES" -o \
+-name "README" -o \
+-name "*.targ" -o \
+-name "*.TARG" -o \
+-name ".tm_properties" -o \
+-name ".TM_PROPERTIES" -o \
+-name "*.ts" -o \
+-name "*.TS" -o \
+-name "*.tst" -o \
+-name "*.TST" -o \
+-name "*.txt" -o \
+-name "*.TXT" -o \
+-name "*.xml" -o \
+-name "*.XML" -o \
+-name "*.yaml" -o \
+-name "*.YAML" -o \
+-name "*.yml" -o \
+-name "*.YML" \
 \) -exec chtag -tc ISO8859-1 {} \;
 
 #UTF-8 files
-#UTF-8 behavior seems broken. Best to leave these as binary instead.
-#echo "Tagging files for UTF-8"
+#UTF-8 does not convert well, Best to leave these as binary instead.
 echo "Tagging files for UTF-8 as binary"
 
 find . \( \
--iname "*.css" -o \
--iname "*.jsx" -o \
--iname "*.html" -o \
--iname "*.tsx" -o \
--iname "*.xlf" \
+-name "*.css" -o \
+-name "*.CSS" -o \
+-name "*.jsx" -o \
+-name "*.JSX" -o \
+-name "*.html" -o \
+-name "*.HTML" -o \
+-name "*.tsx" -o \
+-name "*.TSX" -o \
+-name "*.xlf" -o \
+-name "*.XLF" \
 \) -exec chtag -b {} \;
 
 #Binary files
 echo "Tagging files for binary"
 
 find . \( \
--iname "*.br" -o \
--iname "*.bz2" -o \
--iname "*.crx" -o \
--iname "*.eot" -o \
--iname "*.gif" -o \
--iname "*.gz" -o \
--iname "*.jar" -o \
--iname "*.jpg" -o \
--iname "*.mpg" -o \
--iname "*.mp3" -o \
--iname "*.mp4" -o \
--iname "*.m4v" -o \
--iname "*.ogg" -o \
--iname "*.ogm" -o \
--iname "*.pax" -o \
--iname "*.p12" -o \
--iname "*.pdf" -o \
--iname "*.png" -o \
--iname "*.svg" -o \
--iname "*.ttf" -o \
--iname "*.woff" -o \
--iname "*.woff2" -o \
+-name "*.br" -o \
+-name "*.BR" -o \
+-name "*.bz2" -o \
+-name "*.BZ2" -o \
+-name "*.crx" -o \
+-name "*.CRX" -o \
+-name "*.eot" -o \
+-name "*.EOT" -o \
+-name "*.gif" -o \
+-name "*.GIF" -o \
+-name "*.gz" -o \
+-name "*.GZ" -o \
+-name "*.jar" -o \
+-name "*.JAR" -o \
+-name "*.jpg" -o \
+-name "*.JPG" -o \
+-name "*.mpg" -o \
+-name "*.MPG" -o \
+-name "*.mp3" -o \
+-name "*.MP3" -o \
+-name "*.mp4" -o \
+-name "*.MP4" -o \
+-name "*.m4a" -o \
+-name "*.M4A" -o \
+-name "*.m4v" -o \
+-name "*.M4V" -o \
+-name "*.ogg" -o \
+-name "*.OGG" -o \
+-name "*.ogm" -o \
+-name "*.OGM" -o \
+-name "*.pax" -o \
+-name "*.PAX" -o \
+-name "*.p12" -o \
+-name "*.P12" -o \
+-name "*.pdf" -o \
+-name "*.PDF" -o \
+-name "*.png" -o \
+-name "*.PNG" -o \
+-name "*.svg" -o \
+-name "*.SVG" -o \
+-name "*.ttf" -o \
+-name "*.TTF" -o \
+-name "*.woff" -o \
+-name "*.WOFF" -o \
+-name "*.woff2" -o \
+-name "*.WOFF2" -o \
 -name "zssServer" \
 \) -exec chtag -b {} \;
 
 #Convert to ebcdic
-#find . \( \
-#-iname ".gitmodules" -o \
-#-iname "*.sh" \
-#\) -exec iconv -f iso8859-1 -t 1047 {} \;
+echo "Converting ebcdic files"
+
+find . \( \
+-name ".gitmodules" -o \
+-name *.jcl" -o \
+-name *.JCL" -o \
+-name "*.sh" -o \
+-name "*.SH" \
+\) -exec sh -c "iconv -f iso8859-1 -t 1047 {} > {}.1047" \; -exec mv "{}".1047 "{}" \;
 
 #Ebcdic files
 echo "Tagging files for IBM-1047"
 
 find . \( \
--iname ".gitmodules" -o \
--iname *.jcl" -o \
--iname "*.sh" \
+-name ".gitmodules" -o \
+-name *.jcl" -o \
+-name *.JCL" -o \
+-name "*.sh" -o \
+-name "*.SH" \
 \) -exec chtag -tc 1047 {} \;
 
 
