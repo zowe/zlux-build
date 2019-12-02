f474b7c977bbb90a05d04f13c6d0647f20fd71ac
1000TurquoisePogs <sgrady@rocketsoftware.com>
2019-12-02 18:27:42 -0500

    Consolidate copy from defaults to instance
    Signed-off-by: 1000TurquoisePogs <sgrady@rocketsoftware.com>

--- deploy.sh
+++ deploy.sh
@@ -64,8 +64,7 @@ mkdir -p $zlux_instance_dir/serverConfig
 mkdir -p $zlux_users_dir/ZLUX/pluginStorage
 mkdir -p $zlux_groups_dir/ZLUX/pluginStorage
 
-cp -vr ../zlux-app-server/defaults/* $zlux_instance_dir/serverConfig
-cp -vr ../zlux-app-server/defaults/ZLUX/pluginStorage/* $zlux_instance_dir/ZLUX/pluginStorage
+cp -vr ../zlux-app-server/defaults/* $zlux_instance_dir/
 # This program and the accompanying materials are
 # made available under the terms of the Eclipse Public License v2.0 which accompanies
 # this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
