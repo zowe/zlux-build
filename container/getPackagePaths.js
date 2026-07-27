6999841d9c380282b6eff051434b41c7c36a5041
James Struga <jstruga@rocketsoftware.com>
2026-07-27 14:06:17 -0400

    better error handle
    Signed-off-by: James Struga <jstruga@rocketsoftware.com>

--- container/getPackagePaths.js
+++ container/getPackagePaths.js
@@ -91,6 +91,10 @@ function sortArtifacts(names){
 
 async function artifactory(name, version, artifact){
 	data = await findArtifact(name, version, artifact);
+	if (!data || !data['results'] || data['results'].length === 0) {
+		console.error(`ERROR: AQL search returned no results for ${name} (version: ${version}, artifact: ${artifact}). Is ZLUX_DOWNLOAD_API_TOKEN valid?`);
+		process.exit(1);
+	}
 	sortArtifacts(data)
 	let results = data['results'][0]['repo'] + '/' + data['results'][0]['path'] + '/' + data['results'][0]['name']
 	return results
